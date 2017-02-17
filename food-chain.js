6188c0680e98d4d671a50646673c1e94924eef62
Fay-OK <Farzaneh.orak@gmail.com>
2017-02-16 22:01:38 -0800

    Passed the first two tests

--- food-chain.js
+++ food-chain.js
@@ -1,11 +1,21 @@
 function FoodChain() {
-    
+
     FoodChain.prototype.verse = function (value) {
 
-        var song = 'I know an old lady who swallowed a fly.\nI don\'t know why she swallowed the fly. Perhaps she\'ll die.\n';
+        var animal = {
+            1: "fly",
+            2: "spider"
+        }
+        var firstPart = "I know an old lady who swallowed a " + animal[value] + ".\n";
 
-        if(value ==1 ){
-            return song;
+        var something = "It wriggled and jiggled and tickled inside her.\nShe swallowed the " + animal[value] + " to catch the fly.\n";
+
+        var lastPart = "I don\'t know why she swallowed the fly. Perhaps she\'ll die.\n";
+
+        if (value == 1) {
+            return firstPart + lastPart;
+        } else if (value == 2) {
+            return firstPart + something + lastPart;
         }
     }
 
