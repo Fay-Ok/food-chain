74e2861d58d68b707624422f46cec357d387df49
Fay-OK <Farzaneh.orak@gmail.com>
2017-02-16 22:23:51 -0800

    Passed the third test. Needs refactoring

--- food-chain.js
+++ food-chain.js
@@ -4,18 +4,23 @@ function FoodChain() {
 
         var animal = {
             1: "fly",
-            2: "spider"
+            2: "spider",
+            3: "bird"
         }
         var firstPart = "I know an old lady who swallowed a " + animal[value] + ".\n";
 
-        var something = "It wriggled and jiggled and tickled inside her.\nShe swallowed the " + animal[value] + " to catch the fly.\n";
+        var secondPart = "How absurd to swallow a bird!\nShe swallowed the bird to catch the spider ";
+
+        var spiderFly = " wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\n";
 
         var lastPart = "I don\'t know why she swallowed the fly. Perhaps she\'ll die.\n";
 
         if (value == 1) {
             return firstPart + lastPart;
         } else if (value == 2) {
-            return firstPart + something + lastPart;
+            return firstPart + "It" + spiderFly + lastPart;
+        } else if (value == 3) {
+            return firstPart + secondPart + "that" + spiderFly + lastPart;
         }
     }
 
