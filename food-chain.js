cfc80b4e5d10dbfbde104d331d3e5b981a30d0ae
Fay-OK <Farzaneh.orak@gmail.com>
2017-02-23 08:32:27 -0800

    Refactoring the sings obj

--- food-chain.js
+++ food-chain.js
@@ -11,12 +11,13 @@ function FoodChain() {
             6: "goat"
         };
 
+        var wriggledAndjiggled = "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\n";
         var sings = {
             "spider": "It wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\n",
-            "bird": "How absurd to swallow a bird!\nShe swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\n",
-            "cat": "Imagine that, to swallow a cat!\nShe swallowed the cat to catch the bird.\nShe swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\n",
-            "dog": "What a hog, to swallow a dog!\nShe swallowed the dog to catch the cat.\nShe swallowed the cat to catch the bird.\nShe swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\n",
-            "goat": "Just opened her throat and swallowed a goat!\nShe swallowed the goat to catch the dog.\nShe swallowed the dog to catch the cat.\nShe swallowed the cat to catch the bird.\nShe swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\n"
+            "bird": "How absurd to swallow a bird!\n" + wriggledAndjiggled,
+            "cat": "Imagine that, to swallow a cat!\nShe swallowed the cat to catch the bird.\n" + wriggledAndjiggled,
+            "dog": "What a hog, to swallow a dog!\nShe swallowed the dog to catch the cat.\nShe swallowed the cat to catch the bird.\n" + wriggledAndjiggled,
+            "goat": "Just opened her throat and swallowed a goat!\nShe swallowed the goat to catch the dog.\nShe swallowed the dog to catch the cat.\nShe swallowed the cat to catch the bird.\n" + wriggledAndjiggled
         };
 
         var firstPart = "I know an old lady who swallowed a " + animal[value] + ".\n";
