6289f73c22435f17b1240b36b3d3db38770ce709
Fay-OK <Farzaneh.orak@gmail.com>
2017-02-24 08:20:11 -0800

    Refactored to generate the song

--- food-chain.js
+++ food-chain.js
@@ -11,23 +11,33 @@ function FoodChain() {
             6: "goat"
         };
 
-        var wriggledAndjiggled = "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\n";
+        var swallowdToCatch = "";
+
         var sings = {
-            "spider": "It wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\n",
-            "bird": "How absurd to swallow a bird!\n" + wriggledAndjiggled,
-            "cat": "Imagine that, to swallow a cat!\nShe swallowed the cat to catch the bird.\n" + wriggledAndjiggled,
-            "dog": "What a hog, to swallow a dog!\nShe swallowed the dog to catch the cat.\nShe swallowed the cat to catch the bird.\n" + wriggledAndjiggled,
-            "goat": "Just opened her throat and swallowed a goat!\nShe swallowed the goat to catch the dog.\nShe swallowed the dog to catch the cat.\nShe swallowed the cat to catch the bird.\n" + wriggledAndjiggled
+            "spider": "It wriggled and jiggled and tickled inside her.\n",
+            "bird": "How absurd to swallow a bird!\n",
+            "cat": "Imagine that, to swallow a cat!\n",
+            "dog": "What a hog, to swallow a dog!\n",
+            "goat": "Just opened her throat and swallowed a goat!\n"
         };
 
         var firstPart = "I know an old lady who swallowed a " + animal[value] + ".\n";
         var lastPart = "I don\'t know why she swallowed the fly. Perhaps she\'ll die.\n";
+        var winggledAndjiggled = "that wriggled and jiggled and tickled inside her.\n";
 
+        for (var i = value; i >= 2; i--) {
+
+            if (animal[i - 1] == "spider") {
+                swallowdToCatch += "She swallowed the " + animal[i] + " to catch the " + animal[i - 1] + " " + winggledAndjiggled;
+                continue;
+            }
+            swallowdToCatch += "She swallowed the " + animal[i] + " to catch the " + animal[i - 1] + ".\n";
+        }
 
         if (value == 1) {
             return firstPart + lastPart;
         } else
-            return firstPart + sings[animal[value]] + lastPart;
+            return firstPart + sings[animal[value]] + swallowdToCatch + lastPart;
     }
 
 };
